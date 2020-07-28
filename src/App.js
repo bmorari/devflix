b9157be66646f030448c85caafbf1a609f9b8254
Bruno Morari <brunomorari69@gmail.com>
2020-07-28 09:40:29 -0300

    master

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"Venha aprender front-e  nd!!"}
       />
 
       <Carousel
